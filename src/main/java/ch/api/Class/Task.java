31bc0e00294e91f0a0a42339f84dd858ea527ec8
Andrea Graf <andrea.graf@heig-vd.ch>
2023-12-14 11:11:52 +0100

    Implement the gestion oh Task

--- src/main/java/ch/api/Class/Task.java
+++ src/main/java/ch/api/Class/Task.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Task {
+package ch.api.Class;
+
+public class Task {
+    private static Integer count = 0;
+    public int id;
+    public String title;
+    public String description;
+
+    public Task(String title, String description){
+        id = count++;
+        this.title = title;
+        this.description = description;
+    }
+    public Task(){
+    }
 }
--- src/main/java/ch/api/Controller/TaskController.java
+++ src/main/java/ch/api/Controller/TaskController.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class TaskController {
+package ch.api.Controller;
+
+import ch.api.Class.Task;
+import io.javalin.http.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TaskController {
+    private ConcurrentHashMap<Integer, Task> tasks = new ConcurrentHashMap<Integer, Task>();
+    private int lastId = 0;
+    public void getAll(Context ctx){
+        ctx.json(tasks);
+    }
+    public void getOne(Context ctx){
+        int id = Integer.parseInt(ctx.pathParam("id"));
+        ctx.json(tasks.get(id));
+    }
+    public void create(Context ctx){
+        Task user = ctx.bodyAsClass(Task.class);
+        tasks.put(++lastId, user);
+        ctx.status(201);
+
+    }
+    public void delete(Context ctx) {
+        int id = Integer.parseInt(ctx.pathParam("id"));
+        tasks.remove(id);
+        ctx.status(204);
+    }
+    public void update(Context ctx) {
+        int id = Integer.parseInt(ctx.pathParam("id"));
+        Task task = ctx.bodyAsClass(Task.class);
+        tasks.put(id, task);
+        ctx.status(200);
+    }
 }
