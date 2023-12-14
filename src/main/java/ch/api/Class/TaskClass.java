cfbf5000da8affba5b96950a4296cbde006d6a93
Andrea Graf <andrea.graf@heig-vd.ch>
2023-12-14 11:36:50 +0100

    Create class TaskClass and add id in Task

--- src/main/java/ch/api/Class/TaskClass.java
+++ src/main/java/ch/api/Class/TaskClass.java
@@ -1,17 +1,18 @@
 package ch.api.Class;
 
-public class Task {
+public class TaskClass {
 
     private static int counter = 0;
-    public int id;
+    int id;
     public String title;
     public String description;
 
-    public Task(String title, String description){
+    public TaskClass(String title, String description) {
         id = ++counter;
         this.title = title;
         this.description = description;
     }
-    public Task(){
+
+    public TaskClass() {
     }
 }
